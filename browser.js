b620a7cb060ff04f1739141ba4f44ef179b436ec
OPM002 Lwin Ko Ko <lwinkoko@op-j.jp>
2018-03-07 16:31:48 +0630

    Second office

---
+++ browser.js
@@ -0,0 +1,8 @@
+function browserDetails()
+{
+  document.write("Browser code name : " + navigator.appCodeName + "<br>");
+  document.write("Browser name : " + navigator.appName + "<br>");
+  document.write("Browser version : " + navigator.appVersion + "<br>");
+  document.write("Browser platform : " + navigator.platform + "<br>");
+  document.write("Browser details : " + navigator.userAgent + "<br>");
+}
